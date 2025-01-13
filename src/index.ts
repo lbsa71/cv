a3f243c430a467bcfe641e6fbc01ae2702b7fb2e
“lbsa71” <lbsa71@lbsa71.net>
2025-01-13 14:46:09 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
 import path from "path";
+import fs from "fs";
+import { promisify } from "util";
+import { parse } from "json5";
+import unzipper from "unzipper";
+import sharp from "sharp";
 import { loadAndTransformData } from "./services/data/transformationService";
+import { Config } from "./types";
 import { generateCV } from "./services/pdf/pdfGenerator";
 
 async function main() {
   try {
     console.log("Loading and transforming data...");
-    cons config = // TBD: Load config from json file provided via arguments
+    const args = process.argv.slice(2);
+    const configFileName = args.find(arg => arg.endsWith('.json'));
+    const zipFileName = args.find(arg => arg.endsWith('.zip'));
+    const imageFileName = args.find(arg => /\.(jpg|jpeg|png)$/.test(arg));
+
+    let config: Config | undefined;
+
+    if (configFileName) {
+      console.log("Loading configuration from JSON file...");
+      const configFileContent = await promisify(fs.readFile)(configFileName, 'utf8');
+      config = parse(configFileContent);
+    }
+
+    if (zipFileName) {
+      console.log("Extracting ZIP file...");
+      await fs.createReadStream(zipFileName)
+        .pipe(unzipper.Extract({ path: 'extracted' }))
+        .promise();
+    }
+
+    if (imageFileName) {
+      console.log("Processing image file...");
+
+    }
     const data = await loadAndTransformData(config);
 
     console.log("Generating PDF...");
