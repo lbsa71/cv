7c0ad5544db16813103f5ade0cc1dd29ce107881
“lbsa71” <lbsa71@lbsa71.net>
2025-01-13 15:37:54 +0100

    sip

--- shared/src/services/processRawData.ts
+++
@@ -1,9 +0,0 @@
-import { transformData } from "./transformationService";
-import type { Config } from "@cv/shared";
-
-export function processRawData(rawData: Record<string, any>, config?: Config) {
-    console.log("Raw data processed:", rawData);
-    const transformedData = transformData(rawData, config);
-    console.log("Transformed data:", transformedData);
-    return transformedData;
-}
